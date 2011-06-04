6c9160c9b437eb485540b28acc7a1bcf72fc60f0
John Croisant <jacius@gmail.com>
2011-06-04 12:27:31 -0700

    Wrap in `(progn ... )

--- smarttabs.el
+++ smarttabs.el
@@ -32,22 +32,24 @@
     ad-do-it))
 
 (defmacro smart-tabs-advice (function offset)
-  (defvaralias offset 'tab-width)
-  `(defadvice ,function (around smart-tabs activate)
-     (cond
-      (indent-tabs-mode
-       (save-excursion
-         (beginning-of-line)
-         (while (looking-at "\t*\\( +\\)\t+")
-           (replace-match "" nil nil nil 1)))
-       (setq tab-width tab-width)
-       (let ((tab-width fill-column)
-             (,offset fill-column)
-             (wstart (window-start)))
-         ad-do-it
-         (set-window-start (selected-window) wstart)))
-      (t
-       ad-do-it))))
+  `(progn
+     (defvaralias ',offset 'tab-width)
+     (defadvice ,function (around smart-tabs activate)
+       (cond
+        (indent-tabs-mode
+         (save-excursion
+           (beginning-of-line)
+           (while (looking-at "\t*\\( +\\)\t+")
+             (replace-match "" nil nil nil 1)))
+         (setq tab-width tab-width)
+         (let ((tab-width fill-column)
+               (,offset fill-column)
+               (wstart (window-start)))
+           (unwind-protect
+               (progn ad-do-it)
+             (set-window-start (selected-window) wstart))))
+        (t
+         ad-do-it)))))
 
 (smart-tabs-advice c-indent-line c-basic-offset)
 (smart-tabs-advice c-indent-region c-basic-offset)
