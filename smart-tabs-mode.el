f8ee2126a55d5d55f9562ae3aa5da6f366f0531f
Daniel Dehennin <daniel.dehennin@baby-gnu.org>
2012-08-03 14:54:36 +0200

    Add a "License" header

--- smart-tabs-mode.el
+++ smart-tabs-mode.el
@@ -13,6 +13,8 @@
 
 ;; This file is not part of GNU Emacs.
 
+;;; License:
+
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 2 of the License, or
