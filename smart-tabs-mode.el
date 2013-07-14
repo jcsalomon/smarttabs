bb54ae8e6f827f82a5d7bac403c7d36f5dd88578
Joel C. Salomon <joelcsalomon@gmail.com>
2013-07-13 22:57:15 -0400

    Rearrange documentation a bit

--- smart-tabs-mode.el
+++ smart-tabs-mode.el
@@ -37,6 +37,11 @@
 
 ;; This package provide a semantic way of using tab characters in
 ;; source code: tabs for indentation, spaces for alignment.
+;;
+;; It is derived from <http://www.emacswiki.org/emacs/SmartTabs>
+;; with modifications by the various authors listed above.
+;;
+;; Modification history is at <https://github.com/jcsalomon/smarttabs>.
 
 ;;; Installation:
 
@@ -109,10 +114,6 @@
 ;; NOTE: All language support must be added before the call to
 ;;      `smart-tabs-insinuate'.
 
-;; This package is derived from <http://www.emacswiki.org/emacs/SmartTabs>
-;; with modifications by the various authors listed above.
-;;
-;; Modification history is at <https://github.com/jcsalomon/smarttabs>.
 
 ;;; Code:
 
