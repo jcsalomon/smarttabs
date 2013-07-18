57f6b6d3f0bc642a1ef4ae811a6ac05426e1944d
Joel C. Salomon <joelcsalomon@gmail.com>
2013-07-18 12:37:31 -0400

    Adapt Matt’s doc updates to Joel’s grammatical taste

--- smart-tabs-mode.el
+++ smart-tabs-mode.el
@@ -45,15 +45,13 @@
 
 ;;; Installation:
 
-;; The prefered method of installation of smart-tabs-mode is through
+;; The easiest and preferred way to install smart-tabs-mode is to use
 ;; the package available on MELPA.
 ;;
+;; Manual installation:
 ;;
-;; Manual Installation:
-;;
-;; Save smart-tabs-mode.el to a directory on your load-path and add the
-;; following to your configuration file:
-;;
+;; Save smart-tabs-mode.el to a a directory on your load-path
+;; (e.g., ~/.emacs.d/elisp), then add the following to your .emacs file:
 ;;
 ;;  (autoload 'smart-tabs-mode "smart-tabs-mode"
 ;;    "Intelligently indent with tabs, align with spaces!")
@@ -62,38 +60,39 @@
 ;;  (autoload 'smart-tabs-insinuate "smart-tabs-mode")
 ;;
 
-;;; Enabling smart-tabs-mode for Languages:
+;;; Enabling smart-tabs-mode within language modes:
 
-;; The prefered method of enabling smart-tabs-mode for languages is
-;; through the use of the smart-tabs-insinuate function as follows:
+;; As of version 1.0 of this package, the easiest and preferred way to
+;; enable smart-tabs-mode is with the smart-tabs-insinuate function;
+;; for example,
 ;;
-;; (smart-tabs-insinuate 'c 'c++ 'java)
+;;  (smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
+;;                        'ruby 'nxml)
 ;;
+;; will enable smart-tabs-mode with all supported language modes.
 ;;
-;; Enabling Support for Languages Manually: (not recommended)
+;; (See below for instructions on adding additional language support.)
+;;
+;; The old method of manually enabling smart-tabs-mode is still
+;; available, but is no longer recommended; smart-tabs-insinuate
+;; wraps the functionality below in a convenient manner.
+;;
+;; For reference, the basic manual method looks like this:
 ;;
 ;;  (add-hook 'MODE-HOOK 'smart-tabs-mode-enable)
 ;;  (smart-tabs-advice INDENT-FUNC TAB-WIDTH-VAR)
 ;;
 ;; Note that it might be preferable to delay calling smart-tabs-advice
-;; until after the major mode is loaded and evaluated:
-;;
-;; (eval-after-load 'MODE-FEATURE
-;;   '(smart-tabs-advice INDENT-FUNC TAB-WIDTH-VAR))
-;;
-;; Or:
+;; until after the major mode is loaded and evaluated, so the lines
+;; above would be better written like this:
 ;;
 ;;  (add-hook 'MODE-HOOK (lambda ()
 ;;                         (smart-tabs-mode-enable)
 ;;                         (smart-tabs-advice INDENT-FUNC TAB-WIDTH-VAR)))
 ;;
-;; Here are some specific examples for a few popular languages which
-;; can be enabled by 'smart-tab-insinuate':
+;; Here are some specific examples for a few popular languages:
 ;;
-;;  ;; Load all the following in one pass
-;;  (smart-tabs-insinuate 'c 'javascript 'cperl 'python 'ruby)
-;;
-;;  ;; C/C++
+;;  ;; C
 ;;  (add-hook 'c-mode-hook 'smart-tabs-mode-enable)
 ;;  (smart-tabs-advice c-indent-line c-basic-offset)
 ;;  (smart-tabs-advice c-indent-region c-basic-offset)
@@ -109,12 +108,8 @@
 ;;  ;; Python
 ;;  (add-hook 'python-mode-hook 'smart-tabs-mode-enable)
 ;;  (smart-tabs-advice python-indent-line-1 python-indent)
-;;
-;;  ;; Ruby
-;;  (add-hook 'ruby-mode-hook 'smart-tabs-mode-enable)
-;;  (smart-tabs-advice ruby-indent-line ruby-indent-level)
 
-;;; Adding Language Support
+;;; Adding language support
 
 ;; Language support can be added through the use of the macro
 ;; `smart-tabs-add-language-support'. Pass in the symbol you wish
@@ -129,7 +124,6 @@
 ;;
 ;; NOTE: All language support must be added before the call to
 ;;      `smart-tabs-insinuate'.
-
 
 ;;; Code:
 
